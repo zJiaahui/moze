a09dc042b87ed4b61ff937bccd53b373167d6bcb
zjiaahui <211535176@qq.com>
2021-03-27 13:10:05 +0800

    添加客户端宽度大于500提示

--- src/main.js
+++ src/main.js
@@ -4,5 +4,11 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
+
+window.onload = () => {
+    if (document.documentElement.clientWidth > 500) {
+        window.alert("请使用手机打开，以保证预览效果")
+    }
+}
