989488912f2cf32705a7637c87ea15a9a1aba0a5
zjiaahui <211535176@qq.com>
2021-03-27 17:57:05 +0800

    引入字体图标创建字体图标组件

---
+++ src/layout/components/Icon.vue
@@ -0,0 +1,27 @@
+<template>
+  <svg class="icon" aria-hidden="true">
+    <use :xlink:href="`#i-${name}`"></use>
+  </svg>
+</template>
+
+<script>
+import "../../assets/font/iconfont"
+export default {
+  name: "Icon",
+  props: {
+    name:{
+      type:String
+    }
+  }
+}
+</script>
+
+<style scoped>
+.icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
