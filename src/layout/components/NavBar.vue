cf9d9b6fbacfc7549355a8cecc6cea3d30adb1ef
zjiaahui <211535176@qq.com>
2021-03-27 20:00:07 +0800

    导航栏添加切换样式

--- src/layout/components/NavBar.vue
+++ src/layout/components/NavBar.vue
@@ -1,17 +1,40 @@
 <template>
   <div class="nav">
-    <router-link to="/total"><icon name="total"></icon></router-link>
-    <router-link to="/add"><icon name="addrecord"></icon></router-link>
-    <router-link to="/chart"><icon name="chart"></icon></router-link>
+    <router-link to="/total" >
+      <div @click="handlerClick" data-name="total" :class="{active:activeName=='total'}">
+        <icon name="total"></icon>
+      </div>
+    </router-link>
+    <router-link to="/add">
+      <div @click="handlerClick" data-name="add" :class="{active:activeName=='add'}">
+      <icon name="addrecord"></icon>
+      </div>
+    </router-link>
+    <router-link to="/chart">
+      <div @click="handlerClick" data-name="chart" :class="{active:activeName=='chart'}">
+      <icon name="chart"></icon>
+      </div>
+    </router-link>
   </div>
 </template>
 
 <script>
 import icon from "./Icon"
+
 export default {
   name: "navBar",
-  components:{
+  data(){
+    return{
+      activeName:"total"
+    }
+  },
+  components: {
     icon
+  },
+  methods: {
+    handlerClick(e) {
+     this.activeName= e.currentTarget.getAttribute("data-name")
+    }
   }
 }
 </script>
@@ -27,5 +50,8 @@ export default {
   font-size: 34px;
   color: #fff;
   background-color: #23222e;
+  & .active{
+    color: #37b6ce;
+  }
 }
 </style>
