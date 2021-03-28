b8c2388eb02b198e3c004a701ccd23f7c57fcdce
zjiaahui <211535176@qq.com>
2021-03-28 14:02:20 +0800

    安装vuex搭建vueX文件结构

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from "vue"
+import vueX from "vuex"
+
+Vue.use(vueX)
+
+export default  new vueX.Store({
+    state:{
+
+    },
+    actions:{
+
+    },
+    mutations:{
+
+    },
+    getters:{
+
+    }
+})
